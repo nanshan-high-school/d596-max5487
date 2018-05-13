f0cce9f224f61be7c7c791010eac37079bf346b4
max5487 <32165088+max5487@users.noreply.github.com>
2018-05-13 23:11:49 +0800

    Update b596炸彈放習羊.cpp

--- b596炸彈放習羊.cpp
+++ b596炸彈放習羊.cpp
@@ -1,12 +1,12 @@
-//第9行好像還有問題
 #include <iostream>
 using namespace std;
 int main() {
-	//ntonnt => next to or not next to
-	int	ntonnt[4], bomb[10] = {1,2,3,4,5,6,7,8,9,\0}, times;
+	int	ntonnt[4] = {0}, bomb[10] = {1,2,3,4,5,6,7,8,9}, times;
 	cin >> times;
 	for ( int i = 0; i < times; i++){
-		cin >> ntonnt;
+		for (int o = 0; o <times; o++){
+		cin >> ntonnt[o]w;
+		}
 		for ( int q = 1; q < 10 ;q++){
 			if ( bomb[q - 1] == ntonnt[0] || bomb[q + 1] == ntonnt[0] ||bomb[q - 3] == ntonnt[0] || bomb[q + 3] == ntonnt[0] ){
 				if ( bomb[q - 1] != ntonnt[1] || bomb[q + 1] != ntonnt[1] ||bomb[q - 3] != ntonnt[1] || bomb[q + 3] != ntonnt[1]){
