4c3fab5a7bfc9769797f628e0b7b166b6ad995d8
max5487 <32165088+max5487@users.noreply.github.com>
2018-05-13 23:24:02 +0800

    Update b596炸彈放習羊.cpp

--- b596炸彈放習羊.cpp
+++ b596炸彈放習羊.cpp
@@ -5,20 +5,20 @@ int main() {
 	cin >> times;
 	for ( int i = 0; i < times; i++){
 		for (int o = 0; o <times; o++){
-		cin >> ntonnt[o]w;
+		cin >> ntonnt[o];
 		}
 		for ( int q = 1; q < 10 ;q++){
 			if ( bomb[q - 1] == ntonnt[0] || bomb[q + 1] == ntonnt[0] ||bomb[q - 3] == ntonnt[0] || bomb[q + 3] == ntonnt[0] ){
 				if ( bomb[q - 1] != ntonnt[1] || bomb[q + 1] != ntonnt[1] ||bomb[q - 3] != ntonnt[1] || bomb[q + 3] != ntonnt[1]){
 					if ( bomb[q - 1] != ntonnt[2] || bomb[q + 1] != ntonnt[2] ||bomb[q - 3] != ntonnt[2] || bomb[q + 3] != ntonnt[2]){
-						cout << bomb[q] << " ";
+						cout << bomb[q] << " "<<endl;
 					}
 					else {
-						cout << "Empty"; 
+						cout << "Empty"<<endl; 
 					}
-				cout << "Empty";
+				cout << "Empty"<<endl;
 				}
-			cout << "Empty";
+			cout << "Empty"<<endl;
 			}
 		}
 	}
